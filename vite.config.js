8310072a3b4de73b703bff160870f4c8d3ba3f25
Ron <tararar98@gmail.com>
2026-06-13 20:15:03 +0700

    Update: ปรับปรุง main.jsx และ vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", 
-})
+  build: {
+    // ปรับขนาดแจ้งเตือนให้มากขึ้นเพื่อไม่ให้กวนใจ
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        // แยกไลบรารีใหญ่ๆ ออกจากไฟล์หลัก
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // แยกไฟล์จาก node_modules ไปไว้ที่ chunk ชื่อ vendor
+          }
+        }
+      }
+    }
+  }
+});
